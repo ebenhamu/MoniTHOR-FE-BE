fdb4a6ab24f1abfcc703dc5c60900917cb4d421e
ebenhamu <ebenhamu@gmail.com>
2025-02-01 20:56:54 +0200

    update

--- MoniTHOR--Project-FE/static/register.js
+++ MoniTHOR--Project-FE/static/register.js
@@ -18,7 +18,7 @@ async function Register(UserName, Password1, Password2) {
     console.log("in function");
 
     try {
-        let response = await fetch('http://34.217.100.13:5000/BEregister', {
+        let response = await fetch('http://localhost:5000/BEregister', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
